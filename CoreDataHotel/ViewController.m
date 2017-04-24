445f1699808595fcdce3e8037e2b76707f731498
Annie Ton-Nu <annieqton@gmail.com>
2017-04-24 14:48:17 -0700

    final hour of lecture

--- CoreDataHotel/ViewController.m
+++ CoreDataHotel/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AutoLayout.h"
 
 @interface ViewController ()
 
@@ -16,8 +17,54 @@
 
 -(void)loadView{
     [super loadView];
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self setupLayout];
 }
+
+
+-(void)setupLayout{
+    
+    float navBarHeight = CGRectGetHeight(self.navigationController.navigationBar.frame);
+    
+    UIButton *browseButton = [self createButtonWithTitle:@"Browse"];
+    UIButton *bookButton = [self createButtonWithTitle:@"Book"];
+    UIButton *lookupButton = [self createButtonWithTitle:@"Look Up"];
+
+    browseButton.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:0.75 alpha:1.0];
+    
+    [AutoLayout leadingConstraintFrom:browseButton toView:self.view];
+    [AutoLayout trailingConstraintFrom:browseButton toView:self.view];
+    
+    [AutoLayout equalHeightConstraintFromView:browseButton toView:self.view withMultiplier:0.33];
+    
+    [browseButton addTarget:self action:@selector(browseButonSelected) forControlEvents:UIControlEventTouchUpInside];
+    
+}
+
+
+-(void)browseButonSelected{
+    NSLog(@"Work on this for lab!");
+}
+
+
+-(UIButton *)createButtonWithTitle:(NSString *)title{
+    
+    UIButton *button = [[UIButton alloc]init];
+    
+    [button setTitle:title forState:UIControlStateNormal];
+    
+    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    
+    [button setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    [self.view addSubview:button];
+    
+    return button;
+}
+
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
